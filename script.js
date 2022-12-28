f52a9dc1490912fa830365c0b205aa28b8b46ff0
Mihai0505 <118223998+Mihai0505@users.noreply.github.com>
2022-12-28 17:25:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-
+  function myFunction2() {
+    var x = document.getElementById("contact");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
   function updateTimer() {
-    future = Date.parse("nov 26, 2022 00:09:00");
+    future = Date.parse("apr 26, 2023 00:09:00");
     now = new Date();
     diff = future - now;
 
@@ -41,3 +48,70 @@ document.addEventListener("DOMContentLoaded", function(){
       }
   }
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }//else{ entry.target.classList.remove('show'); }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show2');
+    }//else{ entry.target.classList.remove('show'); }
+  });
+});
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show3');
+    }else{ entry.target.classList.remove('show3'); }
+  });
+});
+
+const hiddenElements3 = document.querySelectorAll('.hidden3');
+hiddenElements3.forEach((el) => observer3.observe(el));
+
+
+window.addEventListener("load", function(){
+  setTimeout(
+    function open(event){
+      document.querySelector(".popup").style.display = "block";
+    },
+    0
+  )
+});
+
+document.querySelector(".close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+});
+
+function progres(x) {
+  if (x !== 0 && document.querySelectorAll(".softprogress").length > 0 && document.querySelectorAll(".softprogress [code-softprogress]").length > 0) {
+    document.querySelectorAll(".softprogress [code-softprogress]").forEach((el) => runsoftprogress(el));
+  }else if(x === 0){
+    document.querySelectorAll(".softprogress [code-softprogress]").forEach((el) => caca(el));
+      
+  }};
+
+function runsoftprogress(el) {
+  el.className = "run-softprogress";
+  el.setAttribute("style",`--run-softprogress:${el.getAttribute("code-softprogress")}%;`
+  );}
+
+  function caca(el) {
+      el.className = "run-softprogress2";
+    }
